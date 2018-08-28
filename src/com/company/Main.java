8442b00e9ebe9ea110305035eccc991d28eed063
bensonresseler <bresseler.vdab@gmail.com>
2018-08-28 14:29:31 +0200

    Inheritance oefn 7 - Override toString() (String.join?)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,66 +9,25 @@ import java.util.Objects;
 public class Main {
 
     public static void main(String[] args) {
-        Bank bank = new Bank();
-        String[] rekeningnummers = {"000-0000011-11", "000-0000022-22", "000-0000033-33"};
-        for (String rekeningnummer : rekeningnummers) {
-            bank.addRekening(new BankRekening(rekeningnummer));
-        }
-        System.out.println("Probeer dubbele rekening toe te voegen:");
-        try {
-            bank.addRekening(new BankRekening(rekeningnummers[0]));
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage());
-        }
-        System.out.println("Output: ");
-        for (BankRekening bankrek : bank) {
-            System.out.println(bankrek.getRekeningnummer());
-        }
+        MyArrayList lijst = new MyArrayList();
+        lijst.add("Karen");
+        lijst.add("Kristel");
+        lijst.add("Kathleen");
+        System.out.println(lijst);
     }
 }
-
-class Bank implements Iterable<BankRekening> {
-    private ArrayList<BankRekening> rekeningen = new ArrayList<>();
-
-    public void addRekening(BankRekening rekening) {
-        if (bestaatBankRekening(rekening))
-            throw new IllegalArgumentException(String.format("Rekening %s bestaat al", rekening.getRekeningnummer()));
-        rekeningen.add(rekening);
-    }
-
-    public boolean bestaatBankRekening(BankRekening rekening) {
-        return rekeningen.contains(rekening);
-    }
-
+class MyArrayList extends ArrayList<String> {
     @Override
-    public Iterator<BankRekening> iterator() {
-        Iterator<BankRekening> it = rekeningen.iterator();
-        return it;
-    }
-}
-
-class BankRekening {
-    private String rekeningnummer;
-
-    public BankRekening(String rekeningnummer) {
-        this.rekeningnummer = rekeningnummer;
-    }
-
-    public String getRekeningnummer() {
-        return rekeningnummer;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BankRekening that = (BankRekening) o;
-        return Objects.equals(rekeningnummer, that.rekeningnummer);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(rekeningnummer);
+    public String toString() {
+        String aantalNamen = "";
+        for (int i = 0; i < size(); i++) {
+            if (i == size()-1){
+                aantalNamen += String.format("%s", get(i));
+            } else {
+                aantalNamen += String.format("%s;", get(i));
+            }
+        }
+        String resultaat = "{" + aantalNamen + "}";
+        return resultaat;
     }
 }
