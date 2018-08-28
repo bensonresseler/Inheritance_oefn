53b8b4b2966e83b02aae006d1a068ae097cf4cfd
bensonresseler <bresseler.vdab@gmail.com>
2018-08-28 13:23:31 +0200

    Inheritance oefn 4 - klasse Persoon2 aanmaken, leeftijd berekenen + override toString().

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,31 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Zangeres karen = new Zangeres("Karen", "Damen");
-        System.out.println(karen);
-    }
-}
-class Zangeres extends BasisPersoon{
-    public Zangeres(String voornaam, String achternaam) {
-        super(voornaam, achternaam);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Deze zangeres heet %s %s", getVoornaam(), getAchternaam());
+        ArrayList<Persoon2> personen = new ArrayList<>();
+        personen.add(new Persoon2("Karen", "Damen", LocalDate.of(1974, 10, 28)));
+        personen.add(new Persoon2("Kristel", "Verbeke", LocalDate.of(1975, 12, 10)));
+        personen.add(new Persoon2("Kathleen", "Aerts", LocalDate.of(1978, 6, 18)));
+        for(Persoon2 p: personen){
+            System.out.println(p);
+        }
     }
 }
 
-class BasisPersoon{
+class Persoon2 {
     private String voornaam;
     private String achternaam;
+    private LocalDate geboortedatum;
 
-    public BasisPersoon(String voornaam, String achternaam) {
+    public Persoon2(String voornaam, String achternaam, LocalDate geboortedatum) {
         this.voornaam = voornaam;
         this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
     }
 
     public String getVoornaam() {
@@ -35,8 +36,14 @@ class BasisPersoon{
         return achternaam;
     }
 
+    public int  getLeeftijd() {
+        LocalDate vandaag = LocalDate.now();
+        Period periode = Period.between(geboortedatum,vandaag);
+        return periode.getYears();
+    }
+
     @Override
     public String toString() {
-        return String.format("Deze persoon heet %s %s", voornaam, achternaam);
+        return String.format("%s %s is %d jaar oud.", voornaam, achternaam, getLeeftijd());
     }
 }
