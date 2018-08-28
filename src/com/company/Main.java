a7ccf107b819d1726c31f7f9e14a5168ba60696f
bensonresseler <bresseler.vdab@gmail.com>
2018-08-28 14:02:56 +0200

    Inheritance oefn 6 - Iterator nodig van de lijst bankrekeningen, want je hebt de bankrekeningen nodig, je moet dus de lijst bankrekeningen kunnen overlopen.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,65 +3,72 @@ package com.company;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> zangeressen = new ArrayList<>();
-
-        zangeressen.add("Karen");
-        zangeressen.add("Kristel");
-        zangeressen.add("Kathleen");
-
-        String tezoeken = "Kathleen";
-        if (zangeressen.contains(tezoeken)){
-            System.out.printf("%s staat in de eerste lijst.%n", tezoeken);
-        }else{
-            System.out.printf("%s staat niet in de eerste lijst.%n", tezoeken);
+        Bank bank = new Bank();
+        String[] rekeningnummers = {"000-0000011-11", "000-0000022-22", "000-0000033-33"};
+        for (String rekeningnummer : rekeningnummers) {
+            bank.addRekening(new BankRekening(rekeningnummer));
         }
-
-        ArrayList<Zangeres> zangeressen2 = new ArrayList<>();
-
-        zangeressen2.add(new Zangeres("Karen"));
-        zangeressen2.add(new Zangeres("Kristel"));
-        zangeressen2.add(new Zangeres("Kathleen"));
-
-        Zangeres tezoeken2 = new Zangeres("Kathleen");
-        if (zangeressen2.contains(tezoeken2)){
-            System.out.printf("%s staat in de tweede lijst.%n", tezoeken2);
-        }else{
-            System.out.printf("%s staat niet in de tweede lijst.%n", tezoeken2);
+        System.out.println("Probeer dubbele rekening toe te voegen:");
+        try {
+            bank.addRekening(new BankRekening(rekeningnummers[0]));
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Output: ");
+        for (BankRekening bankrek : bank) {
+            System.out.println(bankrek.getRekeningnummer());
         }
     }
 }
-class Zangeres{
-    private String voornaam;
 
-    public Zangeres(String voornaam) {
-        this.voornaam = voornaam;
+class Bank implements Iterable<BankRekening> {
+    private ArrayList<BankRekening> rekeningen = new ArrayList<>();
+
+    public void addRekening(BankRekening rekening) {
+        if (bestaatBankRekening(rekening))
+            throw new IllegalArgumentException(String.format("Rekening %s bestaat al", rekening.getRekeningnummer()));
+        rekeningen.add(rekening);
     }
 
-    public String getVoornaam() {
-        return voornaam;
+    public boolean bestaatBankRekening(BankRekening rekening) {
+        return rekeningen.contains(rekening);
     }
 
     @Override
-    public String toString() {
-        return voornaam;
+    public Iterator<BankRekening> iterator() {
+        Iterator<BankRekening> it = rekeningen.iterator();
+        return it;
+    }
+}
+
+class BankRekening {
+    private String rekeningnummer;
+
+    public BankRekening(String rekeningnummer) {
+        this.rekeningnummer = rekeningnummer;
+    }
+
+    public String getRekeningnummer() {
+        return rekeningnummer;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Zangeres zangeres = (Zangeres) o;
-        return Objects.equals(voornaam, zangeres.voornaam);
+        BankRekening that = (BankRekening) o;
+        return Objects.equals(rekeningnummer, that.rekeningnummer);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(voornaam);
+        return Objects.hash(rekeningnummer);
     }
 }
